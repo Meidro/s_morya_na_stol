c9fd7d30ed9c60223708b80594166793abd8bda2
Evgenii Minibaev <e.minibaev@worksolutions.ru>
2023-01-30 00:11:16 +0300

    сделал счетчик

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import './App.css';
+import {useState} from 'react';
 
 export const App = () => {
-   return <div></div>;
+   const [count, setCount] = useState(0);
+
+   return (
+      <div>
+         <h1>{count}</h1>
+         <button onClick={() => setCount((prev) => prev + 1)}>Плюс 1</button>
+         <button onClick={() => setCount((prev) => prev - 1)}>Минус 1</button>
+      </div>
+   );
 };
